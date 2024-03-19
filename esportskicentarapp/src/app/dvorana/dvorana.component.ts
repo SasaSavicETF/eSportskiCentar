b65aa7363e949ae26a75183dcc026d45aa7d85df
ignjatic01 <mica.ignjatic01@gmail.com>
2024-03-19 20:33:17 +0100

    BackEnd komponenta za dvoranu zapoceta (get i post)

---
+++ esportskicentarapp/src/app/dvorana/dvorana.component.ts
@@ -0,0 +1,153 @@
+import { Component, OnInit } from '@angular/core';
+import { Dvorana } from '../models/dvorana';
+import { DvoranaService } from './dvorana.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import { Grad } from '../models/grad';
+import { GradService } from '../grad/grad.service';
+
+@Component({
+  selector: 'app-dvorana',
+  templateUrl: './dvorana.component.html',
+  styleUrl: './dvorana.component.css'
+})
+export class DvoranaComponent implements OnInit
+{
+  public dvoranas: Dvorana[] = [];
+  addVisible: boolean = false;
+  editVisible: boolean = false;
+  deleteVisible: boolean = false;
+
+  public editDvorana: Dvorana | undefined;
+  public delDvorana: Dvorana | undefined;
+  public delIdDvorana: number = -1;
+
+  public nazivDvoraneInput: string = "";
+
+  grads: Grad[] = [];
+  selectedGrad: Grad | undefined;
+
+  constructor(private dvoranaService: DvoranaService, private gradService: GradService) { }
+
+
+  ngOnInit(): void 
+  {
+      this.getDvoranas();
+      this.getGrads();
+  }
+
+  public getGrads(): void
+  {
+    this.gradService.getGrads().subscribe(
+      (response: Grad[]) =>
+      {
+        this.grads = response;
+      },
+      (error: HttpErrorResponse) =>
+      {
+        alert(error.message);
+      }
+    );
+  }
+
+  public getDvoranas(): void
+  {
+    this.dvoranaService.getDvoranas().subscribe(
+      (response: Dvorana[]) => 
+      {
+        this.dvoranas = response;
+      },
+      (error: HttpErrorResponse) =>
+      {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onAddDvorana(addForm: NgForm): void
+  {
+    this.addVisible = false;
+    this.dvoranaService.addDvorana(addForm.value).subscribe(
+      (response: Dvorana) =>
+      {
+        this.getDvoranas();
+      },
+      (error: HttpErrorResponse) =>
+      {
+        alert(error.message);
+      }
+    );
+    addForm.reset();
+  }
+
+  public onUpdateDvorana(dvorana: Dvorana): void
+  {
+    this.editVisible = false;
+    this.dvoranaService.updateDvorana(dvorana).subscribe(
+      (response: Dvorana) =>
+      {
+        this.getDvoranas();
+      },
+      (error: HttpErrorResponse) =>
+      {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteDvorana(idDvorana: number): void
+  {
+    this.deleteVisible = false;
+    this.dvoranaService.deleteDvorana(idDvorana).subscribe(
+      (response: void) => {
+        this.getDvoranas();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public searchDvoranas(key: string): void
+  {
+    const results: Dvorana[] = [];
+    for(const dvorana of this.dvoranas)
+    {
+      if(dvorana.nazivDvorane.toLowerCase().indexOf(key.toLowerCase()) !== -1)
+      {
+        results.push(dvorana);
+      }
+    }
+    this.dvoranas = results;
+    if(!key)
+    {
+      this.getDvoranas();
+    }
+  }
+
+  showAddDialog() 
+  {
+    this.addVisible = true;
+  }
+
+  showEditDialog(dvorana: Dvorana) 
+  {
+    if(this.editVisible == false)
+    {
+      this.editVisible = true;
+      this.editDvorana = { ...dvorana };
+    }
+  }
+
+  showDeleteDialog(dvorana: Dvorana) 
+  {
+    this.deleteVisible = true;
+    this.delDvorana = { ...dvorana };
+    this.delIdDvorana = this.delDvorana.idDvorana;
+  }
+
+  closeDeleteDialog()
+  {
+    this.deleteVisible = false;
+  }
+}
---
+++ esportskicentarapp/src/app/models/dvorana.ts
@@ -0,0 +1,12 @@
+import { Grad } from "./grad";
+
+export interface Dvorana
+{
+    idDvorana: number;
+    nazivDvorane: string;
+    kapacitet: number;
+    duzina: number;
+    sirina: number;
+    info: string;
+    grad: Grad;
+}
