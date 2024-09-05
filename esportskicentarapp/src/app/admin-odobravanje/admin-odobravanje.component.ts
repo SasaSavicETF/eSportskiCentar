8e57374d7782f9e92ae49d57e9a869547decec7f
ignjatic01 <mica.ignjatic01@gmail.com>
2024-09-05 11:25:31 +0200

    Napravljena opcija odobravanja

--- esportskicentarapp/src/app/admin-odobravanje/admin-odobravanje.component.ts
+++ esportskicentarapp/src/app/admin-odobravanje/admin-odobravanje.component.ts
@@ -4,6 +4,8 @@ import { CardModule } from 'primeng/card';
 import { Dogadjaj } from '../models/dogadjaj';
 import { DogadjajService } from '../dogadjaj/dogadjaj.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { EmailService } from '../services/email.service';
+import { Email } from '../models/email';
 
 @Component({
   selector: 'app-admin-odobravanje',
@@ -16,7 +18,7 @@ export class AdminOdobravanjeComponent implements OnInit
   public dogadjajs: Dogadjaj[] = [];
   public allDogadjajs: Dogadjaj[] = [];
 
-  constructor(private dogadjajService: DogadjajService, private messageService: MessageService) { }
+  constructor(private dogadjajService: DogadjajService, private messageService: MessageService, private emailService: EmailService) { }
 
   ngOnInit(): void 
   {
@@ -59,6 +61,30 @@ export class AdminOdobravanjeComponent implements OnInit
 
   //LOGIKA ZA IZMJENU DOZVOLE
 
+  public onUpdateDogadjaj(dogadjaj: Dogadjaj): void 
+  {
+    dogadjaj.odobren = true;
+
+    this.dogadjajService.updateDogadjaj(dogadjaj).subscribe(
+      (response: Dogadjaj) => {
+        this.messageService.add({ severity: 'success', summary: 'Uspješna izmjena', detail: 'Dogadjaj je odobren' });
+        console.log('EMAIL ', response.klijent.email);
+        if(response.klijent.email !== null)
+        {
+          const email = new Email(response.klijent.email, `Vaša rezervacija događaja za teren ${response.teren.nazivTerena} za datum ${this.dateToString(this.normalizeDate(this.stringToDate(response.dnevniRaspored.datum)))} je odobrena! \nZa detaljnije informacije, pogledajte stranicu Moji događaji unutar web aplikacije.`);
+
+          this.emailService.sendEmail(email).subscribe(response => {
+            console.log('Email sent.');
+          });
+        }
+        this.loadDogadjajs();
+      },
+      (error: HttpErrorResponse) => {
+        this.messageService.add({ severity: 'error', summary: 'Greška', detail: 'Greška u odobravanju dogadjaja' });
+      }
+    );
+  }
+
   //LOGIKA ZA BRISANJE
 
   //OBE UKLJUCUJU SLANJE MEJLA
@@ -77,6 +103,72 @@ export class AdminOdobravanjeComponent implements OnInit
       }
     }
     return true;
+  }
+
+  public stringToDate(dateString: string): Date 
+  {
+    const [day, month, year] = dateString.split('.').map(Number);
+    return new Date(year, month - 1, day); // Mjeseci su 0-indeksirani u JavaScriptu
+  }
+
+
+  public dateToString(date: Date): string 
+  {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Mjeseci su 0-indeksirani u JavaScriptu
+    const year = date.getFullYear();
+    
+    return `${day}.${month}.${year}`;
+  }
+
+
+
+  public normalizeDate(date1: Date): Date
+  {
+    let dan: number = date1.getDate();
+    let mjesec: number = date1.getMonth();
+    let godina: number = date1.getFullYear();
+
+    if (date1.getMonth() == 1) 
+    {
+      if (date1.getDate() == 28) {
+          if ((date1.getFullYear() % 4 == 0 && date1.getFullYear() % 100 != 0) || date1.getFullYear() % 400 == 0) {
+            dan = 29;
+          } else {
+            dan = 1;
+            mjesec = 2;
+          }
+      } else if (date1.getDate() == 29) {
+          dan = 1;
+          mjesec = 2;
+      }
+    }
+    else if (date1.getDate() == 30 && (date1.getMonth() == 3 || date1.getMonth() == 5 || date1.getMonth() == 8 || date1.getMonth() == 10)) 
+    {
+      dan = 1;
+      mjesec = date1.getMonth() + 1;
+    }
+    else if (date1.getDate() == 31) 
+    {
+      if (date1.getMonth() == 11) {
+          dan = 1;
+          mjesec = 0;
+          godina = date1.getFullYear();
+      } else {
+        dan = 1;
+        mjesec = date1.getMonth() + 1;
+      }
+    }
+    else
+    {
+      dan = date1.getDate() + 1;
+    }
+
+    date1.setDate(dan);
+    date1.setMonth(mjesec);
+    date1.setFullYear(godina);
+
+    return date1;
   }
 
 
