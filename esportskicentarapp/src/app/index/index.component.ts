1051ab061ded460986902a62e6e4cdaeb7bf0e94
ignjatic01 <mica.ignjatic01@gmail.com>
2024-09-06 17:32:53 +0200

    Upgrade index page-a

--- esportskicentarapp/src/app/index/index.component.ts
+++ esportskicentarapp/src/app/index/index.component.ts
@@ -20,15 +20,50 @@ export class IndexComponent implements OnInit, OnDestroy
 
 import { Component, OnInit, OnDestroy, Inject, PLATFORM_ID, ViewEncapsulation } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { Teren } from '../models/teren';
+import { Dvorana } from '../models/dvorana';
+import { TerenService } from '../teren/teren.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent 
+export class IndexComponent implements OnInit
 {
+  public terens: Teren[] = [];
 
+  ngOnInit(): void 
+  {
+    this.getTerens();
+  }
 
+  constructor(private terenService: TerenService) { }
+
+  public getTerens(): void
+  {
+    this.terenService.getTerens().subscribe(
+      (response: Teren[]) => 
+      {
+        let counter = 0;
+        const filteredTerens: Teren[] = [];
+        for(let teren of response)
+        {
+          if(counter == 4)
+          {
+            break;
+          }
+          filteredTerens.push(teren);
+          counter++;
+        }
+        this.terens = filteredTerens;
+      },
+      (error: HttpErrorResponse) =>
+      {
+        alert(error.message);
+      }
+    );
+  }
 }
 
