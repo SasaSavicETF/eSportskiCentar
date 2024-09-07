4654214274f4991dc91e5042d137edc4be8a7957
ignjatic01 <mica.ignjatic01@gmail.com>
2024-09-07 15:03:11 +0200

    Index page nadam se gotov

--- esportskicentarapp/src/app/index/index.component.ts
+++ esportskicentarapp/src/app/index/index.component.ts
@@ -24,6 +24,8 @@ import { Teren } from '../models/teren';
 import { Dvorana } from '../models/dvorana';
 import { TerenService } from '../teren/teren.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Upravnik } from '../models/upravnik';
+import { UpravnikService } from '../upravnik/upravnik.service';
 
 @Component({
   selector: 'app-index',
@@ -33,13 +35,38 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class IndexComponent implements OnInit
 {
   public terens: Teren[] = [];
+  public upravniks: Upravnik[] = [];
 
   ngOnInit(): void 
   {
     this.getTerens();
+    this.getUpravniks();
   }
 
-  constructor(private terenService: TerenService) { }
+  constructor(private terenService: TerenService, private upravnikService: UpravnikService) { }
+
+  public getUpravniks(): void
+  {
+    this.upravnikService.getUpravniks().subscribe(
+      (response: Upravnik[]) => {
+        let counter = 0;
+        const filteredUpravniks: Upravnik[] = [];
+        for(let upravnik of response)
+        {
+          if(counter == 4)
+          {
+            break;
+          }
+          filteredUpravniks.push(upravnik);
+          counter++;
+        }
+        this.upravniks = filteredUpravniks;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    );
+  }
 
   public getTerens(): void
   {
